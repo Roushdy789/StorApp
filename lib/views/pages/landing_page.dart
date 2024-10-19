ec0c7f093f42002497776e6de2b73c2179add8e4
Ahmed Rousdhy <aroushdy115@gmail.com>
2024-10-19 16:37:31 +0300

    [ENHANCEMENT] add the landing page to save the status of the logged in user

--- lib/views/pages/landing_page.dart
+++ lib/views/pages/landing_page.dart
@@ -1,10 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:storeapp/services/auth.dart';
+import 'package:storeapp/views/pages/auth_page.dart';
+import 'package:storeapp/views/pages/bottom_Navbar.dart';
+import 'package:storeapp/views/pages/home.dart';
 
 class LandingPage extends StatelessWidget {
   const LandingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final auth = Provider.of<AuthBase>(context, listen: false);
+    return StreamBuilder<dynamic>(
+      stream: auth.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          final user = snapshot.data;
+          if (user == null) {
+            return const AuthPage();
+          } else {
+            return const Home();
+          }
+        }
+        return const CircularProgressIndicator();
+      },
+    );
   }
 }
